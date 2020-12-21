1c4c198208877d24bfbaf60ad3b399d393b0081c
Frank F <frank1999ipod@gmail.com>
2020-12-21 04:28:35 +0200

    Importer
    Created script that imports data from csv to elasticsearch.

---
+++ import_from_csv.py
@@ -0,0 +1,48 @@
+from elasticsearch import Elasticsearch, helpers
+import csv
+import re
+
+
+def import_movies():
+    es = Elasticsearch()
+
+    with open('movies.csv', encoding='utf-8') as datafile:
+        datareader = csv.DictReader(datafile, delimiter=',', quotechar='"')
+        movies = []
+        for row in datareader:
+            movie = {'_index': 'movies', '_type': 'movie', '_id': int(row['movieId']),
+                     'genres': row['genres'].split('|') if row['genres'] != '(no genres listed)' else []}
+
+            regex = re.search(r'(.*) \((.*)\)', row['title'], re.M)
+            if regex:
+                movie['title'] = regex.group(1)
+                movie['year'] = regex.group(2)
+            else:
+                movie['title'] = row['title']
+                movie['year'] = ''
+
+            movies.append(movie)
+
+    helpers.bulk(es, movies)
+
+
+def import_ratings():
+    es = Elasticsearch()
+
+    with open('ratings.csv', encoding='utf-8') as datafile:
+        datareader = csv.DictReader(datafile, delimiter=',', quotechar='"')
+        ratings = []
+        for n, row in enumerate(datareader):
+            rating = {'_index': 'ratings', '_type': 'rating', '_id': int(n), 'userID': int(row['userId']),
+                      'movieID': int(row['movieId']), 'rating': float(row['rating']),
+                      'timestamp': int(row['timestamp'])}
+
+            ratings.append(rating)
+
+    #print(ratings[0])
+    helpers.bulk(es, ratings)
+
+
+if __name__ == '__main__':
+    import_movies()
+    import_ratings()
